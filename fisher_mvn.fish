e88f954d109278c33501a470b12c01df652a1bef
Andriy Kulynyak <kulynyak@gmail.com>
2016-02-07 00:16:32 +0200

    first commit

---
+++ fisher_mvn.fish
@@ -0,0 +1,6 @@
+#
+#   brew install gnu-sed --default-names
+#
+if not available mvn
+  echo "Please install 'maven' first!"; exit 1
+end
---
+++ mvn.config.fish
@@ -0,0 +1,33 @@
+# source: https://gist.github.com/benc
+# 	https://gist.github.com/benc/a9b74c537f667a8dd28e#file-mvn-load
+# Maven colored output
+# ====================
+#
+# NOTE: on OSX, install GNU sed:
+#
+#   brew install gnu-sed --default-names
+#
+# if not available mvn
+#   echo "Please install 'maven' first!"; return 1
+# end
+
+set -x BOLD (tput bold)
+set -x UNDERLINE_ON (tput smul)
+set -x UNDERLINE_OFF (tput rmul)
+set -x TEXT_BLACK (tput setaf 0)
+set -x TEXT_RED (tput setaf 1)
+set -x TEXT_GREEN (tput setaf 2)
+set -x TEXT_YELLOW (tput setaf 3)
+set -x TEXT_BLUE (tput setaf 4)
+set -x TEXT_MAGENTA (tput setaf 5)
+set -x TEXT_CYAN (tput setaf 6)
+set -x TEXT_WHITE (tput setaf 7)
+set -x BACKGROUND_BLACK (tput setab 0)
+set -x BACKGROUND_RED (tput setab 1)
+set -x BACKGROUND_GREEN (tput setab 2)
+set -x BACKGROUND_YELLOW (tput setab 3)
+set -x BACKGROUND_BLUE (tput setab 4)
+set -x BACKGROUND_MAGENTA (tput setab 5)
+set -x BACKGROUND_CYAN (tput setab 6)
+set -x BACKGROUND_WHITE (tput setab 7)
+set -x RESET_FORMATTING (tput sgr0)
