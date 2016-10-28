b83a66233f66e22459ff5baeb31f4467c06bc29a
Andriy Kulynyak <kulynyak@gmail.com>
2016-10-28 20:14:00 +0300

    removed deps on available function

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@
 # 	https://gist.github.com/benc/a9b74c537f667a8dd28e#file-mvn-load
 # Maven colored output
 
-if not available mvn
+if not not type -q  "mvn"
   echo "Please install 'maven' first!"
 end
 
